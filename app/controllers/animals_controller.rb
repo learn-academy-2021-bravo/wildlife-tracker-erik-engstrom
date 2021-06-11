507afef0e52e46ee2c60e2435bb6088062364d2e
erik-engstrom <eekareeka@yahoo.com>
2021-06-10 17:49:32 -0700

    added animal sightings to animal name

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -17,9 +17,7 @@ class AnimalsController < ApplicationController
 
   def show 
     animal = Animal.find(params[:id])
-    render json: animal.sightings
-    render json: sightings.where(animal_id:params[:id])
-    
+    render json: animal, include: :sightings
   end
 
   def destroy
